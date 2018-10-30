2265ee90e1da85b9fe3a0e66b97296210d0a0ee4
AidanNelson <AidanNelson@users.noreply.github.com>
2018-10-30 14:21:31 -0400

    add datasets

---
+++ spell/spell-dataset-utils.sh
@@ -0,0 +1,12 @@
+spell run -t K80 \
+    --pip-req ../requirements.txt \
+    "python ../utils/dataset-utils.py --help"
+    # \
+    #   --input_src \
+    #   --max_num_images 100 \
+    #   --output_dir \
+    #   --w 256 --h 256 \
+    #   --centered \
+    #   --face_crop 0.7 \
+    #   --action face \
+    #   --save_mode combined"
