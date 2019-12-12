c88c2f2a8d679c28774daea2866a63276563bf46
AidanNelson <AidanNelson@users.noreply.github.com>
2019-12-12 15:30:41 -0500

    updates to DCGAN notebook

---
+++ spell/spell-dataset-utils.sh
@@ -0,0 +1,11 @@
+spell run -t K80 \
+    --framework tensorflow \
+    "python --version"
+    #   --input_src \
+    #   --max_num_images 100 \
+    #   --output_dir \
+    #   --w 256 --h 256 \
+    #   --centered \
+    #   --face_crop 0.7 \
+    #   --action face \
+    #   --save_mode combined"
